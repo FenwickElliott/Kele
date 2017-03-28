0af18242e78ab45b5214e22dc1ccfdff7b769662
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-28 11:01:54 -0400

    refactored to externalize base URIs

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,12 +7,20 @@ class Kele
   include Roadmap
   include Credentials
 
+  def main_server(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
+  end
+
+  def mock_server(endpoint)
+    "https://private-b3c66-blocapi.apiary-mock.com/api/v1/#{endpoint}"
+  end
+
   def initialize(email=Credentials.email, password=Credentials.password)
     body = {
       :email => email,
       :password => password
     }
-    auth = JSON.parse RestClient.post 'https://www.bloc.io/api/v1/sessions', body
+    auth = JSON.parse RestClient.post main_server('sessions'), body
 
     @auth_token = auth['auth_token']
     @headers = {
@@ -22,15 +30,14 @@ class Kele
   end
 
   def get_me
-      JSON.parse RestClient.get 'https://www.bloc.io/api/v1/users/me', @headers
+      JSON.parse RestClient.get main_server('/users/me'), @headers
   end
 
   def get_mentor_availability
-    response = JSON.parse RestClient.get "https://www.bloc.io/api/v1/mentors/#{get_me["current_enrollment"]['mentor_id']}/student_availability", @headers
-    puts response
+    JSON.parse RestClient.get main_server("mentors/#{get_me["current_enrollment"]['mentor_id']}/student_availability"), @headers
   end
 
   def get_messages
-    puts JSON.parse RestClient.get 'https://www.bloc.io/api/v1/message_threads', @headers
+    JSON.parse RestClient.get main_server('message_threads'), @headers
   end
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,12 +1,20 @@
 require 'rest_client'
 require 'json'
 
+def main_server(endpoint)
+  "https://www.bloc.io/api/v1/#{endpoint}"
+end
+
+def mock_server(endpoint)
+  "https://private-b3c66-blocapi.apiary-mock.com/api/v1/#{endpoint}"
+end
+
 module Roadmap
   def get_roadmap(r_id)
-    JSON.parse RestClient.get "https://www.bloc.io/api/v1/roadmaps/#{r_id}", @headers
+    JSON.parse RestClient.get main_server("roadmaps/#{r_id}"), @headers
   end
 
   def get_checkpoint(c_id)
-    JSON.parse RestClient.get "https://www.bloc.io/api/v1/checkpoints/#{c_id}", @headers
+    JSON.parse RestClient.get main_server("checkpoints/#{c_id}"), @headers
   end
 end
